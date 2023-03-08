82bd445be9ffc4e50ebe16ee0a78c3abefdd1d01
kdadair <82833387+kdadair@users.noreply.github.com>
2023-03-07 21:38:56 -0700

    more algo improvements, invert styling

--- reading_order.py
+++ reading_order.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--issues_path', type=str, help='Path to text file containing issues')
 parser.add_argument('--doc_title', type=str, nargs='?', default='Reading List', help='Desired title of HTML page. Optional.')
 parser.add_argument('--file_name', type=str, nargs='?', default='readinglist', help='Desired name of file. Optional.')
-parser.add_argument('--styling', action='store_true', default=True, help='Apply some basic styling. Defaults to true.')
+parser.add_argument('--no_styling', action='store_true', default=False, help='Removes basic styling. Defaults to False.')
 parser.add_argument('--verbose', action='store_true', default=False, help='Print logging. Defaults to False.')
 args = parser.parse_args()
 
@@ -26,7 +26,7 @@ issues_text = open(path).read()
 
 issues_list = issues_text.split("\n")
 
-def get_mu_url(issue_name):
+def get_mu_hyperlink(issue_name):
 
 	if issue_name:
 		# sleep every iteration of the loop, because both Marvel and DuckDuckGo will get mad at you for bombarding them
@@ -38,61 +38,61 @@ def get_mu_url(issue_name):
 			print(search_string)
 		
 		ddg_results = ddg(search_string, max_results=10)
-		
-		if args.verbose:
-			for result in ddg_results:
-				print(result['href'])
 
 		issue_no = issue_name.split(" ")[-2][1:]
-		for result in ddg_results:
-			comic_url = None
-			if issue_no == result['href'].split("_")[-1]:
-				comic_url = result['href']
-				break
-		
-		if not comic_url:
+		year = issue_name.split(" ")[-1][1:-1]
+		comic_url = None
+
+		if issue_no == "0":
 			comic_url = ddg_results[0]['href']
+		else:
+			for result in ddg_results:
+				if args.verbose:
+					print(result['href'])
+				if issue_no == result['href'].split("_")[-1] and year == result['href'].split("_")[-2]:
+					comic_url = result['href']
+					break
+		
+		digital_comic_id = None
+		if comic_url:
+			print(comic_url)
 
-		print(comic_url)
+			response = requests.get(comic_url)
+			soup = BeautifulSoup(response.content, 'html.parser')
 
-		response = requests.get(comic_url)
-		soup = BeautifulSoup(response.content, 'html.parser')
+			# loop through all <script> tags in the HTML page
+			for script in soup.find_all('script'):
+				script_text = str(script)
 
-		# loop through all <script> tags in the HTML page
-		for script in soup.find_all('script'):
-			script_text = str(script)
+				# search for the digital comic id pattern in the script text
+				match = re.search(r'digital_comic_id: "(\d+)"', script_text)
 
-			digital_comic_id = ""
-
-			# search for the digital comic id pattern in the script text
-			match = re.search(r'digital_comic_id: "(\d+)"', script_text)
-
-			# if the pattern is found, extract the digital comic id and break out of the loop
-			if match:
-				digital_comic_id = match.group(1)
-				break
+				# if the pattern is found, extract the digital comic id and break out of the loop
+				if match:
+					digital_comic_id = match.group(1)
+					break
 		
 		if digital_comic_id:
 			return f"<p><a href=https://read.marvel.com/#/book/{digital_comic_id}>{issue_name}</a></p>"
 		else:
-			return None
+			return f"<p><a>{issue_name} (URL not found) </a></p>"
 	else:
 		return '<p class="empty"></p>'
 
 hyperlink_list = []
 
 for issue in issues_list:
-	issue_url = get_mu_url(issue)
+	issue_hyperlink = get_mu_hyperlink(issue)
 
-	if issue_url:
+	if issue_hyperlink:
 		# add link to url list
-		hyperlink_list.append(issue_url)
+		hyperlink_list.append(issue_hyperlink)
 
 hyperlink_string = '\n'.join(hyperlink_list)
 
 # optional styling, defaulted to on.
-style = None
-if args.styling:
+style = ""
+if not args.no_styling:
 	style = """<style>
 	/* Modern layout */
 	body {
